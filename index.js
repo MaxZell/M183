669c6965421e16c26d8209cd5a7acd2065e09b30
mzelensky <maxim.zelensky@soxes.ch>
2020-11-27 16:07:37 +0100

    mysql connection added

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 ﻿const express = require('express');
+var mysql = require('mysql');
 
+//db connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "daily_todo"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+//node routing
 const app = express();
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend')));
-
+  // app.use(express.static(path.join(__dirname, 'frontend')));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    res.send("Hello");
   });
 }
 
+//port 5000 listening
 app.listen(port, () => console.log(`Listening on port ${port}`));
