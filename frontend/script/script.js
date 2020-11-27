1cda2d2b9f308a668a0d0fc12b99c7702fdab6ac
mzelensky <maxim.zelensky@soxes.ch>
2020-11-27 16:09:11 +0100

    ajax

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,1 +1,7 @@
-﻿
+﻿function xhrPost() {
+    var data = {"name":"John", "time":"2pm"};
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5000/");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+}
