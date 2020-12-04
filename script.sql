c21b086f38d775e5c69ed619b251314b37dd62e3
mzelensky <maxim.zelensky@soxes.ch>
2020-12-04 20:29:11 +0100

    init

--- script.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `ticket` (
-  `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `title` text,
-  `description` text,
-  `status` BOOLEAN NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ script/script.js
@@ -0,0 +1,37 @@
+﻿// XSS exaple
+//<img src='x' onerror='alert("XSS Example")'>
+
+window.onload = loadToDos();
+
+//show todo add div
+function showToDoAdder(){
+    document.getElementById('toDoInput').style.visibility = "visible";
+}
+//hide todo add div
+function hideToDoAdder(){
+    document.getElementById('toDoInput').style.visibility = "hidden";
+}
+
+//https://developer.mozilla.org/ru/docs/Web/API/IndexedDB_API/Using_IndexedDB
+function openDb() {
+    return new Promise(function(resolve, reject) {
+      var request = indexedDB.open();
+      request.onsuccess = () => resolve(request.result);
+    });
+  }
+  
+  function saveToDo() {
+    openDb().then(function(db) {
+       // do stuff with db
+    }).catch(console.warn).finally(function(db) {
+      if(db) db.close();
+    });
+  }
+  
+  function getToDos() {
+    openDb().then(function(db) {
+       // do more stuff with db
+    }).catch(console.warn).finally(function(db) {
+      if(db) db.close();
+    });
+  }
